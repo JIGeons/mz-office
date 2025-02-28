b24763a14dc3a09da98b6df82e5bfbf23435840e
JIGeons <rlrjtlrl@skuniv.ac.kr>
2025-02-28 10:28:06 +0900

    ComponentsPath 생성

---
+++ src/components/ComponentsPath.js
@@ -0,0 +1,43 @@
+// ComponentsPath.js
+
+// Common 컴포넌트
+import Footer from "./Common/Footer";
+import NotFound from "./Common/NotFound";
+import SideBar from "./Common/Sidebar";
+
+// 채팅 관련 컴포넌트
+import ChatGuide from "./Chat/ChatGuide";
+import ChatRequest from "./Chat/ChatRequest";
+import ChatResponse from "./Chat/ChatResponse";
+import Request from "./Chat/Request";
+import RequestButton from "./Chat/RequestButton";
+
+// Dialog 관련 컴포넌트
+import DialogConfirmCancel from "./Dialog/DialogConfirmCancel";
+
+// 로그인 관련 컴포넌트
+import NaverLoginButton from "./Login/NaverLoginButton";
+import NaverLogoutOrDeleteButton from "./Login/NaverLogoutOrDeleteButton";
+
+
+
+export {
+    // Common
+    Footer,
+    NotFound,
+    SideBar,
+
+    // Chat
+    ChatGuide,
+    ChatRequest,
+    ChatResponse,
+    Request,
+    RequestButton,
+
+    // Dialog
+    DialogConfirmCancel,
+
+    // Login
+    NaverLoginButton,
+    NaverLogoutOrDeleteButton,
+};
