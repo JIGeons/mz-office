cbe4d9610c5e298ab75c5ea6fa52190be12ee4d7
JIGeons <rlrjtlrl@skuniv.ac.kr>
2025-03-01 21:35:51 +0900

    footer 추가

--- src/components/Common/Footer.js
+++ src/components/Common/Footer.js
@@ -1,12 +1,32 @@
 import React from "react";
 
+// Image
+import mzLogo from "../../assets/images/mz_logo_white.png";
+import email from "../../assets/images/email.png";
+
 // CSS
 import "../../styles/components/footer.css";
 
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>&copy; {new Date().getFullYear()} MZOffice. All rights reserved.</p>
+            <div className="footer-left">
+                <img src={ mzLogo } alt="mzLogo.png" />
+                <div className="footer-left-text">
+                    <h3>개인정보 처리방침</h3>
+                    <h3>이용약관</h3>
+                </div>
+            </div>
+            <div className="footer-right">
+                <h4>
+                    <img src={ email } alt="email.png" />
+                    문의하기: hyunjeewang@gmail.com
+                </h4>
+                <div>
+                    <p>서비스 제공자 : 비사이드 - 시닙사원 팀</p>
+                    <p>&copy; 2025 시닙사원. All Rights Reserved.</p>
+                </div>
+            </div>
         </footer>
     );
 }
